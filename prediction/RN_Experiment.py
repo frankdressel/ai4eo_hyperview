38012ac13490eacdddb3603ff93017e17b18bfbc
hoff_nl <nils.hoffmann@dlr.de>
2022-03-24 23:34:49 +0100

    introduced RN50 based test

---
+++ prediction/RN_Experiment.py
@@ -0,0 +1,156 @@
+import time
+from typing import Any
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+
+import optax
+import scipy
+from flax.training import checkpoints
+from flax.training import train_state
+
+from prediction.ResNetModel import ResNet50
+from preprocessing.pipeline import Pipeline, Batch, get_data_pipeline, train_test_split, sample_count
+
+
+class ResNetExperiment:
+    class TrainState(train_state.TrainState):
+        batch_stats: Any
+
+    def __init__(self, pipeline: Pipeline, debug=False):
+        self.pipeline = pipeline
+        self.workdir = "workdir"  # TODO - Figure out the usualk experiment management
+
+        self.print_every_epoch = 1
+        self.safe_every_epoch = 10
+
+        self.input_shape = pipeline.input_shape
+        self.debug = debug
+
+        model = ResNet50(num_classes=4)
+
+        init_key = jax.random.PRNGKey(0)
+        variables = model.init({"params": init_key}, jnp.ones(self.input_shape))
+        params = variables["params"]
+        batch_stats = variables["batch_stats"]
+
+        # LR schedule
+        steps_per_epoch = 1190 // 32 # estimate tbh
+        base_learning_rate = 0.05
+        num_epochs = 100
+        warmup_epochs = 10
+        warmup_fn = optax.linear_schedule(
+            init_value=0., end_value=base_learning_rate,
+            transition_steps=warmup_epochs * steps_per_epoch)
+        cosine_epochs = max(num_epochs - warmup_epochs, 1)
+        cosine_fn = optax.cosine_decay_schedule(
+            init_value=base_learning_rate,
+            decay_steps=cosine_epochs * steps_per_epoch)
+        schedule_fn = optax.join_schedules(
+            schedules=[warmup_fn, cosine_fn],
+            boundaries=[warmup_epochs * steps_per_epoch])
+
+        tx = optax.adam(learning_rate=schedule_fn)
+        self.state = ResNetExperiment.TrainState.create(
+            apply_fn=model.apply,
+            params=params,
+            tx=tx,
+            batch_stats=batch_stats,
+        )
+        self.model_loss = lambda pred, label: ((pred - label) ** 2).mean()
+
+        if not debug:
+            self.train_step = jax.jit(self.train_step)
+            self.metrics = jax.jit(self.metrics)
+
+    def train_step(self, state: TrainState, batch: Batch):
+        def loss_fn(params):
+            pred, new_model_state = state.apply_fn(
+                {'params': params, 'batch_stats': state.batch_stats}, batch.image, mutable=['batch_stats'])
+            loss = self.model_loss(pred, batch.label)
+
+            weight_penalty_params = jax.tree_leaves(params)
+            weight_decay = 0.0
+            weight_l2 = sum([jnp.sum(x ** 2)
+                             for x in weight_penalty_params
+                             if x.ndim > 1])
+            weight_penalty = weight_decay * 0.5 * weight_l2
+            loss = loss + weight_penalty
+
+            return loss, (new_model_state, pred)
+
+        grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
+        aux, grad = grad_fn(state.params)
+
+        loss = aux[0]
+        new_model_state, preds = aux[1]
+        new_state = state.apply_gradients(grads=grad, batch_stats=new_model_state["batch_stats"])
+
+        return new_state, loss, preds
+
+    def eval_step(self, batch: Batch):
+        predictions = self.state.apply_fn(
+            {'params': self.state.params, 'batch_stats': self.state.batch_stats},
+            batch.image, mutable=False, train=False)
+        return self.metrics(predictions, batch.label)
+
+    def metrics(self, pred, label):
+        mse = ((pred - label) ** 2).mean()
+        score = mse / self.pipeline.baseline_mse
+        return {
+            "mse": mse,
+            "score": score
+        }
+
+    def save_checkpoint(self):
+        step = int(self.state.step)
+        checkpoints.save_checkpoint(self.workdir, self.state, step, keep=3)
+
+    def train_epochs(self, epochs: int):
+
+        for epoch in range(epochs):
+            train_losses, train_metrics = [], []
+            eval_metrics = []
+            epoch_start = time.time()
+
+            for batch in self.pipeline.train_generator():
+                state, loss, predictions = self.train_step(self.state, batch)
+                metrics = self.metrics(predictions, batch.label)
+                train_losses.append(loss)
+                train_metrics.append(metrics)
+                self.state = state
+
+            for batch in self.pipeline.test_generator():
+                eval_metric = self.eval_step(batch)
+                eval_metrics.append(eval_metric)
+
+            train_summary = {
+                f"train_{k}": v
+                for k, v in jax.tree_map(lambda x: x.mean(), train_metrics).items()
+            }
+            eval_summary = {
+                f"eval_{k}": v
+                for k, v in jax.tree_map(lambda x: x.mean(), train_metrics).items()
+            }
+
+            if epoch % self.print_every_epoch == 0:
+                print(f"Epoch {epoch}, took: {time.time() - epoch_start}")
+                print(f"train metrics: \n \t {train_summary}")
+                print(f"eval metrics: \n \t {eval_summary}")
+
+            if epoch % self.safe_every_epoch == 0:
+                self.save_checkpoint()
+
+
+if __name__ == '__main__':
+    input_shape = (32, 32, 150)
+
+
+    def preprocess_img(img):
+        return jax.image.resize(img, input_shape, "nearest")
+
+    split = train_test_split(sample_count(), 0.3)
+    pipeline = get_data_pipeline(split, batch_size=32, preprocess_img=preprocess_img)
+    experiment = ResNetExperiment(pipeline, False)
+    experiment.train_epochs(100)
---
+++ preprocessing/pipeline.py
@@ -0,0 +1,166 @@
+import typing
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Callable
+
+import jax.numpy as jnp
+import numpy as np
+import pandas as pd
+from flax import struct
+
+DATA_PATH = Path("/home/hoff_nl/datasets/ai4eo/train_data/train_data")
+LABEL_PATH = Path("/home/hoff_nl/datasets/ai4eo/train_data/train_gt.csv")
+
+
+@dataclass
+class Split:
+    train_samples: np.ndarray
+    test_samples: np.ndarray
+
+
+def train_test_split(sample_count: int, test_split) -> Split:
+    idx = np.arange(sample_count)
+    np.random.shuffle(idx)
+    test_samples = int((sample_count * test_split))
+    train_samples = sample_count - test_samples
+
+    return Split(idx[:train_samples], idx[train_samples:])
+
+
+def sample_count() -> int:
+    assert len(pd.read_csv(LABEL_PATH)) == len(list(DATA_PATH.glob("*.npz")))
+    return len(list(DATA_PATH.glob("*.npz")))
+
+
+@dataclass
+class LabelData:
+    train_label_data: np.ndarray  # train samples x attributes
+    test_label_data: np.ndarray  # test samples x attributes
+
+    std_dev: np.ndarray  # attributes
+    mean: np.ndarray  # attributes
+
+    baseline_mse: np.ndarray  # just the average columnwise for score calc
+
+
+def prepare_labels(label_path: Path, split: Split) -> LabelData:
+    labels = pd.read_csv(label_path)
+    label_arr = labels.to_numpy()[:, 1:] # remove idx column
+    train_label_data = label_arr[split.train_samples, :]
+
+    # normalization
+    mean = np.mean(train_label_data)
+    var = np.std(train_label_data)
+
+    label_arr = (label_arr - mean) / var
+
+    baseline_algo = np.mean(label_arr.mean(axis=0))
+    baseline_error = ((label_arr - baseline_algo) ** 2).mean()
+
+    return LabelData(
+        train_label_data,
+        test_label_data=label_arr[split.test_samples, :],
+        std_dev=var,
+        mean=mean,
+        baseline_mse=baseline_error
+    )
+
+
+@dataclass
+class ImageData:
+    train_images: np.ndarray  # array of prescaled images
+    test_images: np.ndarray
+
+    mean: np.ndarray  # channelwise
+    std_var: np.ndarray  # channelwise
+
+
+def prepare_data(data_path: Path, split: Split, preprocess_fn) -> ImageData:
+    images = data_path.glob("*.npz")
+
+    # Load all images to memory, dataset is small enough to not worry about it
+    # Channels last format (transpose)
+    img_data: list[np.ndarray] = [np.ma.MaskedArray(**np.load(str(image))).data.transpose((1, 2, 0)) for image in
+                                  images]
+
+    # probably possible in a more efficient way..
+    train_img_data = np.array(img_data)[split.train_samples]
+    test_img_data = np.array(img_data)[split.test_samples]
+
+    means, variances = [], []
+    for img in train_img_data:
+        # mean axis = [0, 1] -> mean axis [0], mean axis [0]. Np doesnt do reduction along multiple axis
+        means.append(img.mean(axis=0).mean(axis=0))
+        variances.append(img.std(axis=0).mean(axis=0))
+
+    mean = np.array(means).mean(axis=0)
+    var = np.array(variances).mean(axis=0)
+
+    processed_train = []
+    # normalize and preprocess to single size
+    for img in train_img_data:
+        norm = (img - mean) / var
+        preprocessed = preprocess_fn(norm)
+        processed_train.append(preprocessed)
+
+    processed_test = []
+    for img in test_img_data:
+        norm = (img - mean) / var
+        preprocessed = preprocess_fn(norm)
+        processed_test.append(preprocessed)
+
+
+    return ImageData(
+        np.array(processed_train),
+        np.array(processed_test),
+        mean,
+        var
+    )
+
+
+@struct.dataclass
+class Batch:
+    image: jnp.ndarray
+    label: jnp.ndarray
+
+
+@dataclass
+class Pipeline:
+    train_generator: Callable[[], typing.Iterator[Batch]]
+    test_generator: Callable[[], typing.Iterator[Batch]]
+
+    input_shape: tuple[int]
+    baseline_mse: np.ndarray  # for score calc
+
+
+def get_data_pipeline(split: Split, batch_size: int, preprocess_img: Callable):
+    labels = prepare_labels(LABEL_PATH, split)
+    images = prepare_data(DATA_PATH, split, preprocess_img)
+
+    train_samples_count = labels.train_label_data.shape[0]
+    shuffle_idx = np.arange(train_samples_count)
+
+    def train_generator():
+        np.random.shuffle(shuffle_idx)
+
+        # skips the last elements which dont form a complete batch. Should be fine due to shuffling
+        for batch_pos in range(0, train_samples_count, batch_size):
+            train_samples = shuffle_idx[batch_pos:batch_pos + batch_size]
+
+            data_batch = images.train_images[train_samples]
+            label_batch = labels.train_label_data[train_samples]
+
+            yield Batch(data_batch, label_batch)
+
+    def test_generator():
+        for batch_pos in range(0, train_samples_count, batch_size):
+            data_batch = images.train_images[batch_pos:batch_pos + batch_size]
+            label_batch = labels.train_label_data[batch_pos:batch_pos + batch_size]
+
+            yield Batch(data_batch, label_batch)
+
+        # TODO: Just drop last incomplete batch? Or drop + shuffle? Or introduce batch masking+padding
+
+    shape = next(train_generator()).image.shape
+
+    return Pipeline(train_generator, test_generator, input_shape=shape, baseline_mse=labels.baseline_mse)
